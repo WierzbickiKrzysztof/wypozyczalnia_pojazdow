7bff6b8bf46faf8b3f121e8474b0ee0ac86ff1d1
Grzegorz Szczepanik <s38480@s.pwste.edu.pl>
2022-05-18 14:36:29 +0200

    Add Layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<x-layout>
+    <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
+        <div class="col">
+            <div class="d-flex">
+                <div class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-3 bs-icon"><i class="fas fa-car-alt"></i></div>
+                <div class="px-3">
+                    <h4>Samochody osobowe</h4>
+                    <p>Potrzebujesz samochodu?<br>Przyjdź do naszego oddziału aby poznać szczegóły i wypożyczyć wybrany pojazd.</p><a class="link-light" href="#">Zobacz nasze samochody</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="d-flex">
+                <div class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-3 bs-icon"><i class="fas fa-car-side"></i></div>
+                <div class="px-3">
+                    <h4>Skutery</h4>
+                    <p>Skuter wypożyczysz samodzielnie po weryfikacji prawa jazdy kategorii AM w naszym oddziale.<br>Ceny już od 30 zł/dzień.</p><a class="link-light" href="#">Zobacz nasze skutery</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="d-flex">
+                <div class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-3 bs-icon"><i class="fas fa-baby-carriage"></i></div>
+                <div class="px-3">
+                    <h4>Hulajnogi</h4>
+                    <p>Hulajnogi elektryczne wypożyczysz samodzielnie po weryfikacji Twojego dowodu osobistego w naszym oddziale.<br>Już od 15 zł/dzień.</p><a class="link-light" href="#">Zobacz nasze hulajnogi</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
